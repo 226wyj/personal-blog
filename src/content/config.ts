060d849b10beea20a26c2780e9c8629dc229036c
226wyj <308033240@qq.com>
2024-07-22 00:17:33 -0700

    fix algorithm tags

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,20 +1,5 @@
 import { z, defineCollection } from "astro:content";
 
-const algorithmSchema = z.object({
-  title: z.string(),
-  description: z.string(),
-  pubDate: z.coerce.date(),
-  updatedDate: z.string().optional(),
-  heroImage: z.string().optional(),
-  badge: z.string().optional(),
-  tags: z
-    .array(z.string())
-    .refine((items) => new Set(items).size === items.length, {
-      message: "tags must be unique",
-    })
-    .optional(),
-});
-
 const blogSchema = z.object({
   title: z.string(),
   description: z.string(),
@@ -43,11 +28,11 @@ const storeSchema = z.object({
   heroImage: z.string().optional(),
 });
 
-export type AlgorithmSchema = z.infer<typeof algorithmSchema>;
+export type AlgorithmSchema = z.infer<typeof blogSchema>;
 export type BlogSchema = z.infer<typeof blogSchema>;
 export type StoreSchema = z.infer<typeof storeSchema>;
 
-const algorithmCollection = defineCollection({ schema: algorithmSchema });
+const algorithmCollection = defineCollection({ schema: blogSchema });
 const blogCollection = defineCollection({ schema: blogSchema });
 const storeCollection = defineCollection({ schema: storeSchema });
 
